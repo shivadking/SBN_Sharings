6e67b368628b10ee1c15ce5ee4a06e6dfa7b87ce
shivadking <sivablogg@gmail.com>
2014-02-28 21:44:17 +0530

    dd

---
+++ SocialMediaWithAPI/SBN_Sharing_API/SBN_Sharing_API/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SBN_Sharing_API
+//
+//  Created by Mini Mac i72 on 2/21/14.
+//  Copyright (c) 2014 Mini Mac i72. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SocialMediaWithAPI/SBN_Sharing_API/SBN_Sharing_API/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SBN_Sharing_API
+//
+//  Created by Mini Mac i72 on 2/21/14.
+//  Copyright (c) 2014 Mini Mac i72. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
